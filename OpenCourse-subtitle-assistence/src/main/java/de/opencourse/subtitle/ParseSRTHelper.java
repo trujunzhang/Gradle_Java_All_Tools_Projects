f613dd73546cbcbd8b33978b5ea1c82d3c4269f0
wanghaogithub720 <wanghaomspace@gmail.com>
2014-11-11 11:57:51 +0800

    init

--- OpenCourse-subtitle-assistence/src/main/java/de/opencourse/subtitle/ParseSRTHelper.java
+++ OpenCourse-subtitle-assistence/src/main/java/de/opencourse/subtitle/ParseSRTHelper.java
@@ -1,5 +1,6 @@
 package de.opencourse.subtitle;
 
+import de.opencourse.subtitle.model.SubtitleInfo;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,6 +11,7 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,16 +20,38 @@ import java.util.regex.Pattern;
  */
 public class ParseSRTHelper {
 
+	private LinkedHashMap<Integer, SubtitleInfo> subtitleInfoLinkedHashMap = new LinkedHashMap<Integer, SubtitleInfo>();
+	private SubtitleInfo subtitleInfo;
+
 	public static void main(String[] args) {
+		ParseSRTHelper parseSRTHelper = new ParseSRTHelper();
+
 		String url = "http://oyc.yale.edu/music/musi-112/lecture-";
+
+		parseSRTHelper.fetchByUrl(url);
+
+		String outPath = "/Volumes/Home/djzhang/Desktop/xxx";
+		parseSRTHelper.saveAll(outPath);
+		int x = 0;
+	}
+
+	private void saveAll(String outPath) {
+
+	}
+
+	private void fetchByUrl(String url) {
 		for (int i = 1; i < 100; i++) {
+			subtitleInfo = null;
 			boolean hasEnd = fetchSubtitleByUrl(String.format("%s%d", url, i));
-			if (hasEnd)
+			if (hasEnd) {
 				break;
+			} else {
+				subtitleInfoLinkedHashMap.put(i, subtitleInfo);
+			}
 		}
 	}
 
-	private static boolean fetchSubtitleByUrl(String url) {
+	private boolean fetchSubtitleByUrl(String url) {
 		Document doc = null;
 		try {
 			doc = Jsoup.connect(url).get();
@@ -52,21 +76,23 @@ public class ParseSRTHelper {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-
 		}
 		return false;
 	}
 
-	private static void getTitle(Document doc) {
+	private void getTitle(Document doc) {
 		Element session_body = doc.getElementById("session_body");
 		Elements h2s = session_body.getElementsByTag("h2");
 		Element first = h2s.first();
 		String html = first.html();
 		System.out.println("title is " + html);
 
+		subtitleInfo = new SubtitleInfo();
+		subtitleInfo.title = html;
+
 	}
 
-	private static void readLines(String html) throws IOException {
+	private void readLines(String html) throws IOException {
 		ByteArrayInputStream stringReader = new ByteArrayInputStream(html.getBytes("UTF8"));
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stringReader, "UTF-8"));
 
@@ -77,7 +103,7 @@ public class ParseSRTHelper {
 	}
 
 	// file: "http://openmedia.yale.edu/projects/courses/fall08/musi112/embed/musi112_01_090408.srt",
-	private static void parseJavascript(String html) {
+	private void parseJavascript(String html) {
 		Pattern p = Pattern.compile("file: \"(.+?).srt\""); // Regex for the
 															// value of the
 															// html
@@ -86,6 +112,7 @@ public class ParseSRTHelper {
 		while (m.find()) {
 			// System.out.println(" reg-0: " + m.group()); // the whole html text
 			String group = m.group(1) + ".srt";
+			subtitleInfo.downloadUrl = group;
 			System.out.println(" subtitle is " + group); // value only
 		}
 	}
---
+++ OpenCourse-subtitle-assistence/src/main/java/de/opencourse/subtitle/model/SubtitleInfo.java
@@ -0,0 +1,12 @@
+package de.opencourse.subtitle.model;
+
+/**
+ * Created by djzhang on 11/11/14.
+ */
+public class SubtitleInfo {
+
+
+    public String title;
+    public String downloadUrl;
+
+}
